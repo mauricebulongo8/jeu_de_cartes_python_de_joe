1704364fa1786db59386a64e4c77e10d8db4b318
mauricebulongo8 <bulungomaurice8@gmail.com>
2026-02-07 18:24:36 +0100

    Add First Files

---
+++ cards.py
@@ -0,0 +1,33 @@
+CARTES = {
+    "DEUX": 2,
+    "TROIS": 3,
+    "QUATRE": 4,
+    "CINQ": 5,
+    "SIX": 6,
+    "SEPT": 7,
+    "HUIT": 8,
+    "NEUF": 9,
+    "DIX": 10,
+    "VALET": 10,
+    "REINE": 10,
+    "ROI": 10,
+    "AS": 10
+}
+
+DEUX = 2
+TROIS = 3
+QUATRE = 4
+CINQ = 5
+SIX = 6
+SEPT = 7
+HUIT = 8
+NEUF = 9
+DIX = 10
+VALET = 10
+REINE = 10
+ROI = 10
+AS = 50
+
+SYMBOLES = ["COEUR", "TREFLE", "PIQUE", "CARREAU"]
+
+JOKERS = ["NOIR", "ROUGE"]
---
+++ main.py
@@ -0,0 +1,5 @@
+from cards import CARTES
+from cards import JOKERS
+from cards import SYMBOLES
+
+from 
